8c7e414a324dde33886c6aaa02ed1094a77332e0
Francesc Rambla <frambla@gmail.com>
2023-03-27 18:02:04 +0200

    Bug solved in laravel-mongodb-cron

--- laravel-mongodb-cron/entrypoint.sh
+++ laravel-mongodb-cron/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+TMPFILE=$( mktemp )
 for i in /etc/cronjobs/*
 do
-   /usr/bin/crontab -u www $i
+   cat $i >> $TMPFILE
 done
+
+/usr/bin/crontab -u www $TMPFILE
+rm $TMPFILE
 
 /usr/sbin/cron -f
